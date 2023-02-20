290686ffd330f673c56cd894e5493be344dc425b
parsajiravand <parsajiravand@gmail.com>
2023-02-20 15:10:03 +0330

    add qr code timer

--- src/components/qr-code/index.vue
+++ src/components/qr-code/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="d-flex flex-column justify-content-center">
+    <!--begin::Heading-->
+    <div class="text-center mb-10">
+      <!--begin::Title-->
+      <h1 class="text-dark mb-3">Sign In With QR Code</h1>
+      <!--end::Title-->
+    </div>
+    <!--begin::Heading-->
     <img
-      src="/media/trader4/sample-qr.png"
+      v-if="!loading"
+      :src="qrCode.url"
       class="mx-auto"
       width="250"
       height="250"
       alt="qr-code"
     />
+    <div v-else class="spinner-border text-light mx-auto my-5" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="mx-auto" v-if="refreshQr">
+      <i
+        class="bi bi-arrow-clockwise text-light fs-1 cursor-pointer"
+        @click="fetchQr"
+      ></i>
+    </div>
     <div>
       <!--begin::Form-->
       <VForm
@@ -74,6 +91,7 @@ import { ErrorMessage, Field, Form as VForm } from "vee-validate";
 import { useAuthStore, type User } from "@/stores/auth";
 import { useRouter } from "vue-router";
 import * as Yup from "yup";
+import moment from "moment";
 
 export default defineComponent({
   components: {
@@ -85,13 +103,21 @@ export default defineComponent({
     const store = useAuthStore();
     const router = useRouter();
     const qrCode = ref({});
+    const loading = ref(false);
 
+    // Fetch QR Code on mounted
     onMounted(() => {
       fetchQr();
+      clearInterval(interval);
     });
+
+    // Fetch QR Code
     const fetchQr = async () => {
+      refreshQr.value = false;
+      loading.value = true;
       // Send login request
       const res = await store.fetchQr();
+      loading.value = false;
       const error = Object.values(store.errors);
 
       if (error.length) {
@@ -110,8 +136,24 @@ export default defineComponent({
       }
       if (res.succeed) {
         qrCode.value = res.results;
-        console.log(res);
+        clearInterval(interval);
+
+        checkTimer();
       }
+    };
+
+    // Timer
+    const startTime = new Date().getTime();
+    let interval = null as any;
+    const refreshQr = ref(false);
+    const checkTimer = () => {
+      interval = setInterval(function () {
+        if (new Date().getTime() - startTime > 60000) {
+          clearInterval(interval); //stop the interval
+          refreshQr.value = true; //show refresh button
+          return;
+        }
+      }, 1000);
     };
 
     const submitButton = ref<HTMLButtonElement | null>(null);
@@ -171,6 +213,9 @@ export default defineComponent({
       onSubmitLogin,
       login,
       submitButton,
+      refreshQr,
+      fetchQr,
+      loading,
     };
   },
 });
