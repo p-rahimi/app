4dcd171a2056b2dbcaf813968544c3e0e6518a88
parsajiravand <parsajiravand@gmail.com>
2023-03-06 14:24:36 +0330

    create global store

---
+++ src/stores/global.ts
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+import Swal from "sweetalert2";
+
+// state
+const errors = ref({});
+
+export function setError(error: any) {
+  errors.value = { ...error };
+}
+
+function showError() {
+  // error handling from server side validation
+  const error = Object.values(errors.value);
+  Swal.fire({
+    text: error[0] as string,
+    icon: "error",
+    buttonsStyling: false,
+    confirmButtonText: "Try again!",
+    heightAuto: false,
+    customClass: {
+      confirmButton: "btn fw-semobold btn-light-danger",
+    },
+  }).then(() => {
+    errors.value = {};
+  });
+}
